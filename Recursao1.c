6c43708aa956c0f822e622e29e2b585da9b636f3
Matheus00082 <maratheus71@gmail.com>
2023-08-27 18:48:07 -0300

    exercicio-recursao-1

---
+++ Recursao1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ImprimeInvertido(char *string);
+void scanner(char *string);
+
+int main()
+{
+    char string[1001];
+    scanner(string);
+    ImprimeInvertido(string);
+    return 0;
+}
+
+void scanner(char *string)
+{
+    int i = 0;
+    while (i < 1001 && scanf("%c", &string[i]) != EOF)
+    {
+        i++;
+    }
+}
+
+void ImprimeInvertido(char *string)
+{
+    static int i = 0, j;
+
+    if (string[i] == '\0')
+    {
+        return;
+    }
+
+    if (string[i] == ' ')
+    {
+        j = i - 1;
+        while (string[j] != ' ' && j >= 0)
+        {
+            printf("%c", string[j]);
+            j--;
+        }
+        printf(" ");
+    }
+    i++;
+    ImprimeInvertido(string);
+}
