0219a77eb33c4387544a0ff17c20f7c43ad0a5c8
Matheus00082 <maratheus71@gmail.com>
2023-09-13 21:56:46 -0300

    main

---
+++ dermato/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include "paciente.h"
+#include "lesao.h"
+#include "data.h"
+#include "constantes.h"
+#include <stdlib.h>
+
+int main()
+{
+    char id;
+    tPaciente regPacientes[QTD_MAX_PACIENTES];
+    tLesao lesao;
+    int qtdPacientes = 0, qtdLesoes = 0, cirurgias = 0, i = 0, mediaIdade = 0, somaIdade = 0;
+
+    while (TRUE)
+    {
+        if (id == 'P')
+        {
+            regPacientes[qtdPacientes] = inicia_paciente();
+            qtdPacientes++;
+        }
+        else if (id == 'L')
+        {
+            qtdLesoes++;
+            lesao = inicia_lesao();
+            if (lesao.porcentagem > PERICULOSIDADE_MAX)
+            {
+                cirurgias++;
+            }
+
+            while (i < qtdPacientes)
+            {
+                associa_lesao(lesao, regPacientes[i]);
+                i++;
+            }
+
+            i = FALSE;
+        }
+        else if (id == 'F')
+        {
+            break;
+        }
+        else
+        {
+            printf("identificacao invalida");
+            exit(1);
+        }
+    }
+
+    i = FALSE;
+
+    while (i < qtdPacientes)
+    {
+        somaIdade += calcularIdade(regPacientes[i].dataNasc);
+        i++;
+    }
+
+    mediaIdade = (somaIdade / qtdPacientes);
+
+    printf("TOTAL PACIENTES: %d\n", qtdPacientes);
+    printf("MEDIA IDADE (ANOS): %d\n", mediaIdade);
+    printf("TOTAL LESOES: %d\n", qtdLesoes);
+    printf("TOTAL CIRURGIAS: %d\n", cirurgias);
+    printf("LISTA DE PACIENTES:\n");
+    ImprimirPacientes(regPacientes, qtdPacientes);
+
+    return 0;
+}
